8215a4ce22d9839b9cbc383c80d26c9aeadc7094
Sajid Kalam <sajidkalam@proton.me>
2024-10-21 22:03:24 +0530

    Delete Assignment1/literacy.py

--- Assignment1/literacy.py
+++
@@ -1,8 +0,0 @@
-''' In a town, the percentage of men is 52. The percentage of total literacy is 48. If total percentage
-of literate men is 35 of the total population, write a program to find the total number of
-illiterate men and women if the population of the town is 80,000'''
-
-pop=80000
-menper=52 #41600
-literper=48
-litmen=35
