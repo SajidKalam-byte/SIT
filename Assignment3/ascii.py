54ebf005f69d2189c897f1b67bff1c81c7b93f9d
Sajid Kalam <sajidkalam@proton.me>
2024-11-24 23:14:56 +0530

    Assignment3/ascii.py

--- Assignment3/ascii.py
+++ Assignment3/ascii.py
@@ -2,3 +2,5 @@
 The ASCII values vary from 0 to 255[chr(110) will print ascii character of the value 110. ord('A')
 will print corresponding ASCII value of 'A']'''
 
+for i in range(0, 256):
+    print(i, chr(i))  
