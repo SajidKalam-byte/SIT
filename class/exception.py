46f9bfcc61bc938f4c2bf38e99d035149a5759f0
Sajid Kalam <sajidkalam@proton.me>
2024-12-03 11:39:46 +0530

    modified:   class/exception.py

--- class/exception.py
+++ class/exception.py
@@ -1,10 +1,17 @@
 class test(Exception):
     pass
+a=int(input("Enter a number: "))
+
 try:
     print("This try block ")
-    raise test
+    if a>0:
+        raise test
+    
 except test:
     print("This except block")
     print("Test is raised")
 
+finally:
+    print("_______This is compulsory execution____")
+    
 print("End of Code")
