ebb502560b71e0400f801f64713dce7f89931a20
Sajid Kalam <sajidkalam@proton.me>
2025-01-19 20:21:25 +0530

    Update locationSwap.py

--- Assignment1/sumDIgit.py
+++ Assignment1/sumDIgit.py
@@ -1,9 +1,7 @@
 '''Two numbers are input through the keyboard into two locations C and D. Write a program to
 interchange the contents of C and D.'''
-num=(int(input("Enter a number: ")))
-sum=0
-while(num>0):
-    a=num%10
-    sum=sum+a
-    num=num//10
-print("Sum of digits is: ",sum)
+c=(int(input("Enter a number: ")))
+d=(int(input("Enter second number: ")))
+c,d=d,c
+print("Content of C is now : ",c)
+print("Content of D is now : ",d)
